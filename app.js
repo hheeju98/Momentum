fbc80711330fd98947f536b3cdf0930778730719
hheeju98 <hheeju98@naver.com>
2022-12-10 00:51:17 +0900

    Events part Two

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 
+const link = document.querySelector("a");
+
 function onLoginSubmit(event){ 
-  event.preventDefault(); // 브라우저의 디폴트 동작 막아줌
-  console.log(event);
+  event.preventDefault(); // 브라우저의 디폴트 동작 막아줌(새로고침 막음)
+  console.log(loginInput.value);
+}
+
+function handleLinkClick(event){
+    event.preventDefault();
+    console.dir(event);
+alert("clicked!"); 
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
+link.addEventListener("click", handleLinkClick);
+
