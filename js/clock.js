b300944014d8f1faa05168adb32d740fbf5e24c8
hheeju98 <hheeju98@naver.com>
2022-12-10 23:58:38 +0900

    PadStart 시간 형식 설정

--- js/clock.js
+++ js/clock.js
@@ -2,7 +2,10 @@ const clock = document.querySelector("h2#clock");
 
 function getClock(){
     const date = new Date();
-    clock.innerText= `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+    const hours = String(date.getHours()).padStart(2, "0");
+    const minutes = String(date.getMinutes()).padStart(2, "0").padStart(2, "0");
+    const seconds = String(date.getSeconds()).padStart(2, "0");
+    clock.innerText= `${hours}:${minutes}:${seconds}`;
 }
-getClock();
-setInterval(getClock, 1000);
+getClock(); // 즉시 실행
+setInterval(getClock, 1000); // 즉시 실행 후 1초마다 실행
