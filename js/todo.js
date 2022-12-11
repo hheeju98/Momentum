ff6488fbf9bbe0d662e40e95135c2ad0a84a3d9f
hheeju98 <hheeju98@naver.com>
2022-12-11 18:37:29 +0900

    Adding ToDos

--- js/todo.js
+++ js/todo.js
@@ -3,10 +3,19 @@ const toDoInput = toDoForm.querySelector("input");
 // const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
+function paintToDo(newTodo){
+const li = document.createElement("li");
+const span = document.createElement("span");
+li.appendChild(span);
+span.innerText = newTodo;
+toDoList.appendChild(li);
+}
+
 function handleToDoSubmit(event){
 event.preventDefault();
 const newTodo = toDoInput.value; // 비우기전에 newTodo에 값 저장
 toDoInput.value = "";
+paintToDo(newTodo);
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
