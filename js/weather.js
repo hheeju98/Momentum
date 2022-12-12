1c9d7d9bc65ce62c6a6e667e6a2bde36846584bc
hheeju98 <hheeju98@naver.com>
2022-12-13 00:25:13 +0900

    Weather API

--- js/weather.js
+++ js/weather.js
@@ -1,7 +1,16 @@
+const weather = document.querySelector("#weather span:first-child");
+const city = document.querySelector("#weather span:last-child");
+const API_KEY ="c6dd83a313a396f0868c4e5c0121923c";
 function onGeoOk(position){
     const lat = position.coords.latitude;
-    const lng = position.coords.longitude;
-console.log("You live in", lat, lng);
+    const lon = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
 }
 function onGeoError(){
 alert("Can't find you. No weather for you.");
